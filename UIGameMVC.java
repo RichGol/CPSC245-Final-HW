6ac7e5676bfdee71e072abb792864aed71458e7d
Rich Goluszka <richardjgoluszka@lewisu.edu>
2018-12-01 00:28:32 -0600

    Removed unnecessary import, added a JLabel to ScorePanel displaying the amount of knives remaining. Try to implement difficulty scaling the target closer or further. ThrownObjectTarget Model will be difficult to scale, TargetPanel View class will be easier.

--- UIGameMVC.java
+++ UIGameMVC.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
